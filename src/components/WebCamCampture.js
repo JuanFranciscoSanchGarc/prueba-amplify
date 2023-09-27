000f190d5f264af14e7912082ca2d5eb37213d87
JuanFranciscoSanchGarc <juanfranciscosanchez@insurnet.es>
2023-09-27 23:33:13 +0200

    usa la camara trasera

--- src/components/WebCamCampture.js
+++ src/components/WebCamCampture.js
@@ -8,18 +8,28 @@ const WebcamCapture = () => {
   const capture = React.useCallback(async () => {
     const imageSrc = webcamRef.current.getScreenshot();
     const url = `https://8dyid462k8.execute-api.eu-west-1.amazonaws.com/main/revisarRiesgo`;
-      const response =  await axios.post(url, {
-        fileKey: 'manolo',
-        type: 'image/jpeg',
-      });
-      console.log("Response from Lambda:", response.data);
+    const response = await axios.post(url, {
+      fileKey: "manolo",
+      type: "image/jpeg",
+    });
+    console.log("Response from Lambda:", response.data);
   }, [webcamRef]);
 
   return (
-    <>
-      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg"/>
+    <div className="webcam-container">
+      {" "}
+      {/* Añadido un contenedor con clase para los estilos */}
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        className="webcam" // Añadida clase para aplicar estilos
+        videoConstraints={{
+          facingMode: "environment", // Especifica usar la cámara trasera
+        }}
+      />
       <button onClick={capture}>Capture photo</button>
-    </>
+    </div>
   );
 };
 
