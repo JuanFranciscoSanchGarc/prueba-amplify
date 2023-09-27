497d77587bfb08d6f7e71ea1084e5a0ab5833349
JuanFranciscoSanchGarc <juanfranciscosanchez@insurnet.es>
2023-09-27 17:46:05 +0200

    webcam

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import "./components/ButtonLambda";
+import "./components/WebCamCampture";
 
 import ButtonLambda from "./components/ButtonLambda";
+import WebcamCapture from "./components/WebCamCampture";
 
 const App = () => {
   return (
     <div>
       <h1>Bienvenido a la revision de tus riesgo</h1>
+      <WebcamCapture></WebcamCapture>
       <ButtonLambda></ButtonLambda>
     </div>
   );
