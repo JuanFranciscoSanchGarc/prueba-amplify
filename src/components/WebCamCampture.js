f47bfcfcb3725786b04bbb5de375c1d1d8dedc7d
JuanFranciscoSanchGarc <juanfranciscosanchez@insurnet.es>
2023-09-27 17:47:59 +0200

    webcam trasera

--- src/components/WebCamCampture.js
+++ src/components/WebCamCampture.js
@@ -1,20 +1,20 @@
-import React, { useRef } from 'react';
-import Webcam from 'react-webcam';
+import React, { useRef } from "react";
+import Webcam from "react-webcam";
 
 const WebcamCapture = () => {
-    const webcamRef = useRef(null);
+  const webcamRef = useRef(null);
 
-    const capture = React.useCallback(() => {
-        const imageSrc = webcamRef.current.getScreenshot();
-        console.log(imageSrc);
-    }, [webcamRef]);
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc);
+  }, [webcamRef]);
 
-    return (
-        <>
-            <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
-            <button onClick={capture}>Capture photo</button>
-        </>
-    );
+  return (
+    <>
+      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" facingMode="environment" />
+      <button onClick={capture}>Capture photo</button>
+    </>
+  );
 };
 
 export default WebcamCapture;
