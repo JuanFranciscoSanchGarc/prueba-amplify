65a6763c711479aa1ec44e9bfcd3b837be3e8cb4
JuanFranciscoSanchGarc <juanfranciscosanchez@insurnet.es>
2023-09-27 17:34:06 +0200

    componente webcam

---
+++ src/components/webCam.js
@@ -0,0 +1,20 @@
+import React, { useRef } from 'react';
+import Webcam from 'react-webcam';
+
+const WebcamCapture = () => {
+    const webcamRef = useRef(null);
+
+    const capture = React.useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        console.log(imageSrc);
+    }, [webcamRef]);
+
+    return (
+        <>
+            <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+            <button onClick={capture}>Capture photo</button>
+        </>
+    );
+};
+
+export default WebcamCapture;
